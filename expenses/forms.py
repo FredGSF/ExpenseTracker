302b77f056a8fc2ae51e98e9b98545fa683e872c
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-02-20 18:37:44 +0000

    a

--- expenses/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Expense
-
-
-class ExpenseForm(forms.ModelForm):
-    class Meta:
-        model = Expense
-        fields = ["amount", "description", "category", "date"]
